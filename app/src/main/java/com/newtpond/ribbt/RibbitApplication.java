478b0e75abd590186b6d92a5a1e49ca72e344ff5
Martin Turjak <martinturjak@gmail.com>
2014-12-30 04:31:34 +0100

    connected with parse.com + continued work on the sign up activity

---
+++ app/src/main/java/com/newtpond/ribbt/RibbitApplication.java
@@ -0,0 +1,17 @@
+package com.newtpond.ribbt;
+
+import android.app.Application;
+import com.parse.Parse;
+
+/**
+ * Created by martinturjak on 12/30/14.
+ */
+public class RibbitApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "Nasr5RWhl5eyrYY5RnWcgDaJq5kU4QRbHlZwy95i", "hq6Da4CNp6yVRzt9F08krQfstMaYWnrMLU0DQIhQ");
+    }
+}

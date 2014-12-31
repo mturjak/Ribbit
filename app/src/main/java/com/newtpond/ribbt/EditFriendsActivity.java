9645ac2a147277e9cf5c8aaeddceb57f3a8cd59a
Martin Turjak <martinturjak@gmail.com>
2014-12-31 05:01:40 +0100

    used ParseRelation and  added functionality to checkmarks: change checked state + save to server

--- app/src/main/java/com/newtpond/ribbt/EditFriendsActivity.java
+++ app/src/main/java/com/newtpond/ribbt/EditFriendsActivity.java
@@ -6,13 +6,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -21,17 +25,24 @@ public class EditFriendsActivity extends ListActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
     protected List<ParseUser> mUsers;
+    protected ParseRelation<ParseUser> mFriendsRelation;
+    protected ParseUser mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_edit_friends);
+
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
 
         setProgressBarIndeterminateVisibility(true);
 
@@ -41,7 +52,7 @@ public class EditFriendsActivity extends ListActivity {
         query.findInBackground(new FindCallback<ParseUser>() {
             @Override
             public void done(List<ParseUser> users, ParseException e) {
-                setProgressBarIndeterminateVisibility(true);
+                setProgressBarIndeterminateVisibility(false);
 
                 if(e == null) {
                     // Success
@@ -59,6 +70,8 @@ public class EditFriendsActivity extends ListActivity {
 
                     setListAdapter(adapter);
 
+                    addFriendCheckmarks();
+
                 } else {
                     Log.e(TAG, e.getMessage());
                     AlertDialog.Builder builder = new AlertDialog.Builder(EditFriendsActivity.this);
@@ -67,6 +80,27 @@ public class EditFriendsActivity extends ListActivity {
                             .setPositiveButton(android.R.string.ok, null);
                     AlertDialog dialog = builder.create();
                     dialog.show();
+                }
+            }
+        });
+    }
+
+    private void addFriendCheckmarks() {
+        mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> friends, ParseException e) {
+                if(e == null) {
+                    for(int i = 0; i < mUsers.size(); i++) {
+                        ParseUser user = mUsers.get(i);
+
+                        for(ParseUser friend: friends) {
+                            if(friend.getObjectId().equals(user.getObjectId())) {
+                                getListView().setItemChecked(i, true);
+                            }
+                        }
+                    }
+                } else {
+                    Log.e(TAG,e.getMessage());
                 }
             }
         });
@@ -92,5 +126,28 @@ public class EditFriendsActivity extends ListActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        if(getListView().isItemChecked(position)) {
+            // add friend
+            mFriendsRelation.add(mUsers.get(position));
+
+        } else {
+            // remove friend
+            mFriendsRelation.remove(mUsers.get(position));
+        }
+
+        mCurrentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        });
     }
 }

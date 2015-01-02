09591ad1b65c9fbffd158b4ddf0f30d3a5b3de63
Martin Turjak <martinturjak@gmail.com>
2015-01-02 03:22:11 +0100

    call external camera wirh an intent and startActivityForResult

--- app/src/main/java/com/newtpond/ribbt/MainActivity.java
+++ app/src/main/java/com/newtpond/ribbt/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
@@ -39,11 +40,19 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    public static final int TAKE_PHOTO_REQUEST = 0;
+    public static final int TAKE_VIDEO_REQUEST = 1;
+    public static final int PICK_PHOTO_REQUEST = 2;
+    public static final int PICK_VIDEO_REQUEST = 3;
+
+
     protected DialogInterface.OnClickListener mDialogListener = new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialog, int which) {
             switch (which) {
-                case 0: // take picture
+                case TAKE_PHOTO_REQUEST:
+                    Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST);
                     break;
                 case 1: // take video
                     break;

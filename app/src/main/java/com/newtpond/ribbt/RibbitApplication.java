186608bf273d63f5d89cbef062abf36e21d3e5fd
Martin Turjak <martinturjak@gmail.com>
2015-01-02 09:35:23 +0100

    DeployGate SDK

--- app/src/main/java/com/newtpond/ribbt/RibbitApplication.java
+++ app/src/main/java/com/newtpond/ribbt/RibbitApplication.java
@@ -1,6 +1,8 @@
 package com.newtpond.ribbt;
 
 import android.app.Application;
+
+import com.deploygate.sdk.DeployGate;
 import com.parse.Parse;
 
 /**
@@ -13,5 +15,7 @@ public class RibbitApplication extends Application {
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "Nasr5RWhl5eyrYY5RnWcgDaJq5kU4QRbHlZwy95i", "hq6Da4CNp6yVRzt9F08krQfstMaYWnrMLU0DQIhQ");
+
+        DeployGate.install(this);
     }
 }
